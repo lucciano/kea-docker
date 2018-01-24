4ba3d3209fac8dd232514c43aa05c7c4faf50a0f
Luciano Andrade <andrade.luciano@gmail.com>
2018-01-23 22:10:44 -0300

    ...

--- get-and-compile.sh
+++ get-and-compile.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 git clone https://github.com/isc-projects/kea.git /opt/kea && cd /opt/kea && git checkout v1_3_0
-cd /opt/kea && autoreconf --install
-cd /opt/kea && ./configure --with-dhcp-mysql --enable-debug  --enable-static-link    --enable-shell 
-cd /opt/kea && make
-cd /opt/kea && make install
+cd /opt/kea
+autoreconf --install
+./configure --with-dhcp-mysql --enable-debug  --enable-static-link    --enable-shell 
+make -j
+make install
